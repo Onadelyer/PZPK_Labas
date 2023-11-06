d9f8f828287d9f449b95a2227320df4082fffc18
Maksym Tokar <tokarmacs63@gmail.com>
2023-11-06 07:41:53 +0200

    Laba5 raw task3

--- Laba5/Tasks/Task3.xaml.cs
+++ Laba5/Tasks/Task3.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,28 @@ namespace Laba5.Tasks
     /// </summary>
     public partial class Task3 : Page
     {
+        private Regex Regex = new Regex(@"\b\w+\b");
+
         public Task3()
         {
             InitializeComponent();
+            UpdateTextBlocks();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string formattedText = Regex.Replace(Input.Text, "#");
+
+            FileHandler.WriteToTextFile("Original.txt", Input.Text.Split('\n'));
+            FileHandler.WriteToTextFile("Formatted.txt", formattedText.Split('\n'));
+
+            UpdateTextBlocks();
+        }
+
+        private void UpdateTextBlocks()
+        {
+            Input.Text = string.Join("\n", FileHandler.ReadFromTextFile("Original.txt"));
+            Output.Text = string.Join("\n", FileHandler.ReadFromTextFile("Formatted.txt"));
         }
     }
 }
