dba8790e368a92a9598d4b6a94b2f54a81b49e90
Onadelyer <tokarmacs63@gmail.com>
2023-11-12 10:39:50 +0200

    laba5-python task3

---
+++ Laba5-Python/fileHandler.py
@@ -0,0 +1,35 @@
+import pickle
+import os
+
+def read_from_text_file(file_path):
+    if not os.path.exists(file_path):
+        # Create the file if it does not exist
+        with open(file_path, 'w'):
+            pass
+
+    # Read from the file
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    return lines
+
+def write_to_text_file(file_path, lines):
+    with open(file_path, 'w') as file:
+        for line in lines:
+            file.write(line)
+
+def write_to_binary_file(file_path, object_to_write):
+    with open(file_path, 'wb') as file:
+        pickle.dump(object_to_write, file)
+
+def read_from_binary_file(file_path):
+    if not os.path.exists(file_path):
+        # Create the file if it does not exist
+        with open(file_path, 'wb'):
+            pass
+
+    try:
+        with open(file_path, 'rb') as file:
+            read_object = pickle.load(file)
+            return read_object
+    except (pickle.UnpicklingError, FileNotFoundError):
+        return None  # or handle the error in your specific way
--- Laba5-Python/main.py
+++ Laba5-Python/main.py
@@ -1,28 +1,135 @@
-from tkinter import Button
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFrame, QVBoxLayout
+from logging import FileHandler
+import random
+import re
+from datetime import datetime
+from datetime import date
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFrame, QTableWidgetItem, QVBoxLayout, QTableWidget
 from Tasks.MainWindow import Ui_MainWindow
 from Tasks.Task1 import Ui_Frame as Ui_Task1
 from Tasks.Task2 import Ui_Frame as Ui_Task2
 from Tasks.Task3 import Ui_Frame as Ui_Task3
 from Tasks.Task4 import Ui_Frame as Ui_Task4
 from Tasks.Task5 import Ui_Frame as Ui_Task5
+import fileHandler
+import json
 
 
 class Task1(QFrame, Ui_Task1):
-
     def __init__(self):
         super().__init__()
+        
+        self.keepersList = []
         self.setupUi(self)
+        self.dataTable.setColumnCount(6)
+        self.dataTable.setColumnWidth(0, 110)
+        self.dataTable.setColumnWidth(1, 110)
+        self.dataTable.setColumnWidth(2, 110)
+        self.dataTable.setColumnWidth(3, 110)
+        self.dataTable.setColumnWidth(4, 110)
+        self.dataTable.setColumnWidth(5, 110)
         self.addKeeperButton.clicked.connect(self.addKeeper)
+        self.sortButton.clicked.connect(self.sortKeepers)
         
+        loaded_keepers = self.getKeepersFromFile("keepers.json")
+        if loaded_keepers is not None:
+            self.keepersList = loaded_keepers
+            self.refreshTable(self.keepersList)
+                
+    def refreshTable(self, listt):
+        rowCount = 0    
+        
+        self.dataTable.clearContents()
+        self.dataTable.setRowCount(0)
+
+    
+        for keeper in listt:
+            rowCount = self.dataTable.rowCount()
+            self.dataTable.insertRow(rowCount)
+            for column, value in enumerate(keeper.values()):
+                self.dataTable.setItem(rowCount, column, QTableWidgetItem(str(value)))
+
+    def sortKeepers(self):
+        current_year = datetime.now().year
+
+        def years_of_employment(employee):
+            year_of_employment = int(employee["Year of employment"])
+            return current_year - year_of_employment
+
+        keepers = [keeper for keeper in self.keepersList if years_of_employment(keeper) > 10]
+        
+        self.refreshTable(keepers)
+
     def addKeeper(self):
+        rowCount = self.dataTable.rowCount()
+        self.dataTable.insertRow(rowCount)
         
+        randomKeeper = self.generateRandomKeeper()
+        
+        self.keepersList.append(randomKeeper);
+        self.writeKeepersToFile(self.keepersList, "keepers.json") 
+        self.refreshTable(self.keepersList)
+
+    def writeKeepersToFile(self, keepers, filename):
+        with open(filename, 'w') as file:
+            json.dump(keepers, file)
+        
+    def getKeepersFromFile(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                keepers = json.load(file)
+            return keepers
+        except FileNotFoundError:
+            print(f"File '{filename}' not found.")
+            return None
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON from file '{filename}'.")
+            return None
+
+    def generateRandomKeeper(self):
+        names = ["Max", "John", "Michael", "Robert", "Daniel", "James", "William"]
+        surnames = ["Tokar", "Doe", "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis"]
+        patronymics = ["Dmytrovych", "Alexandrovich", "Dmitrievich", "Ivanovich", "Nikolaevich", "Sergeyevich", "Vladimirovich"]
+        addresses = ["Storogynets", "123 Main St", "456 Elm St", "789 Oak St", "101 Maple Ave", "111 Pine St"]
+
+        random_name = random.choice(names)
+        random_surname = random.choice(surnames)
+        random_patronymic = random.choice(patronymics)
+        random_address = random.choice(addresses)
+    
+        random_birthday = date(random.randint(1950, 2004), random.randint(1, 12), random.randint(1, 28)).strftime('%d.%m.%Y')
+        random_year_of_employment = random.randint(2000, date.today().year + 1)
+
+        keeper = {
+            'Name': random_name,
+            'Surname': random_surname,
+            'Patronymic': random_patronymic,
+            'Address': random_address,
+            'Birthday': random_birthday,
+            'Year of employment': str(random_year_of_employment)
+        }
+
+        return keeper
     
 class Task2(QFrame, Ui_Task2):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.generateButton.clicked.connect(self.generate)
+        self.inputBox.setPlainText(" ".join(fileHandler.read_from_text_file("task2intput.txt")))
+        self.outputBox.setPlainText(" ".join(fileHandler.read_from_text_file("task2output.txt")))
         
+    def generate(self):
+        pattern = re.compile(r'\b\w*a\w*\b')
+        inputText = self.inputBox.toPlainText()
+        
+        matches = pattern.findall(inputText)
+        matchesResult = " ".join(matches)
+        
+        fileHandler.write_to_text_file("task2intput.txt", inputText.split("\n"))
+        fileHandler.write_to_text_file("task2output.txt", matchesResult.split("\n"))
+        
+        self.outputBox.setPlainText(matchesResult)
+    
 class Task3(QFrame, Ui_Task3):
     def __init__(self):
         super().__init__()
